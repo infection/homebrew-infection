a7d258a267850bc77a5046cd4952f1f576f83515
Pierre du Plessis <pierre@pcservice.co.za>
2019-09-22 11:38:32 +0200

    Update formula for 0.14.1 release

--- Formula/infection.rb
+++ Formula/infection.rb
@@ -1,15 +1,15 @@
 class Infection < Formula
   desc "AST based PHP Mutation Testing Framework"
   homepage "https://infection.github.io"
-  url "https://github.com/infection/infection/releases/download/0.10.3/infection.phar"
-  version "0.10.3"
-  sha256 "662a1ba3a59a8d1ee49e2da22cd003b9c00b4e93749a91785fe4e3f38d4f2ac3"
+  url "https://github.com/infection/infection/releases/download/0.14.1/infection.phar"
+  version "0.14.1"
+  sha256 "0c7f9325d7f6406de2a1fb36c6c7ceba2aa5ffbd74d1f390eb2290f28a857f29"
 
-  depends_on "php71-xdebug" if Formula["php71"].linked_keg.exist?
+  depends_on "php72-xdebug" if Formula["php72"].linked_keg.exist?
 
   resource "pubkey" do
-    url "https://github.com/infection/infection/releases/download/0.10.3/infection.phar.asc"
-    sha256 "1f1235e72ec1bb73dcd79d1973005c25688aa5568c52b5d9d9acb1bc5c8e88d6"
+    url "https://github.com/infection/infection/releases/download/0.14.1/infection.phar.asc"
+    sha256 "32a3bb69b5df83f3b9668125cf10b901df766018b1ef2b91826c835fa1bf414c"
   end
 
   def install
