19d68473a514f65cb7a13331cddf9befe6fee466
Mark van Duijker <mark@duyker.nl>
2024-12-12 16:02:11 +0100

    release 0.29.9

--- Formula/infection.rb
+++ Formula/infection.rb
@@ -1,15 +1,15 @@
 class Infection < Formula
   desc "AST based PHP Mutation Testing Framework"
   homepage "https://infection.github.io"
-  url "https://github.com/infection/infection/releases/download/0.26.2/infection.phar"
-  version "0.26.2"
-  sha256 "dc1063c29880b52978d87621c8e03b83d53ff57338807c335705152ac4ba464e"
+  url "https://github.com/infection/infection/releases/download/0.29.9/infection.phar"
+  version "0.29.9"
+  sha256 "e636f48733e05b8ef8354970ff7077c28b1f3a895bed4cb34f9be8b0c1aa16d5"
 
   depends_on "php" => :test
 
   resource "pubkey" do
-    url "https://github.com/infection/infection/releases/download/0.26.2/infection.phar.asc"
-    sha256 "bacf2698ef48789fee23bd9570eb5466904bbd1e8cf43376e7f97c1f0a27f06d"
+    url "https://github.com/infection/infection/releases/download/0.29.9/infection.phar.asc"
+    sha256 "9d374c84f5a9006c0297530e39ff51bab989613c9434e2f1f6f4fecf58a6f6d1"
   end
 
   def install
